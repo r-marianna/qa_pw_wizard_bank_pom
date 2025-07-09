8245523957ed8746b7592468e6f42f901d541e6a
Marianna Repka <repka.mv@gmail.com>
2025-07-09 23:35:31 +0300

    Update testData

--- src/TestData.js
+++ src/TestData.js
@@ -1,33 +1,30 @@
-const { faker } = require('@faker-js/faker');
+import { faker } from '@faker-js/faker';
 
-module.exports = {
-    person: {
-        firstName: faker.person.firstName(),
-        lastName: faker.person.lastName(),
-        zipCode: faker.location.zipCode(),
-    },
+export const person = {
+    firstName: faker.person.firstName(),
+    lastName: faker.person.lastName(),
+    zipCode: faker.location.zipCode(),
+};
 
-    users: {
-        harry: 'Harry Potter',
-        ron: 'Ron Weasly',
-        albus: 'Albus Dumbledore',
-        neville: 'Neville Longbottom',
-    },
+export const users = {
+    harry: 'Harry Potter',
+    ron: 'Ron Weasly',
+    albus: 'Albus Dumbledore',
+    neville: 'Neville Longbottom',
+};
 
-    currency: {
-        dollar: 'Dollar',
-        pound: 'Pound',
-        rupee: 'Rupee',
-    },
+export const currency = {
+    dollar: 'Dollar',
+    pound: 'Pound',
+    rupee: 'Rupee',
+};
 
-    accountOperations: {
-        amount: faker.number.int({ max: 100 }),
-    },
+export const accountOperations = {
+    amount: faker.number.int({ max: 100 }),
+};
 
-    options: {
-        month: 'short',
-        day: 'numeric',
-        year: 'numeric'
-    },
-
+export const options = {
+    month: 'short',
+    day: 'numeric',
+    year: 'numeric',
 };
